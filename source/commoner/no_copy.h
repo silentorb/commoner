a7a55c560ee4cca9840e81e2c07b0f554734971e
cj <christopherwjohnson@gmail.com>
2016-10-16 21:34:13 -0600

    Wrapped no_copy in a commoner namespace.

--- source/commoner/no_copy.h
+++ source/commoner/no_copy.h
@@ -1,16 +1,19 @@
 #pragma once
 
-class no_copy {
-public:
-    no_copy(no_copy const &) = delete;
-    no_copy &operator=(no_copy const &) = delete;
+namespace commoner {
 
-    no_copy() { }
+  class no_copy {
+  public:
+      no_copy(no_copy const &) = delete;
+      no_copy &operator=(no_copy const &) = delete;
 
-    no_copy(no_copy &&) { }
-};
+      no_copy() {}
 
-class no_copy_minimal {
-public:
-    no_copy_minimal &operator=(no_copy const &) = delete;
-};
+      no_copy(no_copy &&) {}
+  };
+
+  class no_copy_minimal {
+  public:
+      no_copy_minimal &operator=(no_copy const &) = delete;
+  };
+}
